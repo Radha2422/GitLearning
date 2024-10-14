703eedfe1481f1648902cbe1c31a255f5e863330
Radha2422 <jhansiradha2430@gmail.com>
2024-10-14 18:52:14 +0530

    "2nd commit"

---
+++ src/test/java/Gitlearning/Gitlearning/loginandout.java
@@ -0,0 +1,29 @@
+package Gitlearning.Gitlearning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginandout {
+
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		//Login
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		System.out.println("login successful");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']")).click();
+		driver.findElement(By.xpath("//a[@class='oxd-userdropdown-link'and text()='Logout']")).click();
+		System.out.println("logout successful");
+		driver.quit();
+	}
+
+}
